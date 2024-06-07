fb6a17c7c62e6fff64a7018917410cb883e02e1d
yeong-hwan <jghff700@naver.com>
2024-06-07 15:46:00 +0900

    feat: implement portfolio repository

---
+++ demo/src/main/java/com/example/demo/repository/PortfolioRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.Portfolio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
+}
