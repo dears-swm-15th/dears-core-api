1ace7deda6446c91ca3c9940c8f2b0f547aa26f7
yeong-hwan <jghff700@naver.com>
2024-06-24 19:27:54 +0900

    test: resolve merge conflict at portfolio

--- demo/src/test/java/com/example/demo/dummy/DataLoader.java
+++ demo/src/test/java/com/example/demo/dummy/DataLoader.java
@@ -18,42 +18,24 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
         Portfolio portfolio1 = Portfolio.builder()
                 .organization("Organization One")
-<<<<<<< HEAD
-                .region(Portfolio.Region.CHUNGDAM)
-                .introduction("Introduction to Organization One.")
-                .officeHours(Portfolio.OfficeHours.MORNING)
-                .contactInfo("contact@organizationone.com")
-                .image("image1.jpg")
-                .consultationFee(10000)
-=======
                 .region(Region.CHUNGDAM)
                 .introduction("Introduction to Organization One.")
                 .officeHours(OfficeHours.MORNING)
                 .contactInfo("contact@organizationone.com")
                 .image("image1.jpg")
                 .consultationFee(100)
->>>>>>> feat/package-structure
                 .description("Description of services offered by Organization One.")
                 .weddingPhotos("wedding1.jpg")
                 .build();
 
         Portfolio portfolio2 = Portfolio.builder()
                 .organization("Organization Two")
-<<<<<<< HEAD
-                .region(Portfolio.Region.GANGNAM)
-                .introduction("Introduction to Organization Two.")
-                .officeHours(Portfolio.OfficeHours.AFTERNOON)
-                .contactInfo("contact@organizationtwo.com")
-                .image("image2.jpg")
-                .consultationFee(20000)
-=======
                 .region(Region.GANGNAM)
                 .introduction("Introduction to Organization Two.")
                 .officeHours(OfficeHours.AFTERNOON)
                 .contactInfo("contact@organizationtwo.com")
                 .image("image2.jpg")
                 .consultationFee(200)
->>>>>>> feat/package-structure
                 .description("Description of services offered by Organization Two.")
                 .weddingPhotos("wedding2.jpg")
                 .build();
