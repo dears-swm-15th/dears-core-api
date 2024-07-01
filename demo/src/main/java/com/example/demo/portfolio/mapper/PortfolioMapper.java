574569d081a25583e4daa557599315906f6bd7a5
전영서 <20wjsdudtj@gmail.com>
2024-07-01 16:50:21 +0900

    fix/unmapped-target-in-mapper

--- demo/src/main/java/com/example/demo/portfolio/mapper/PortfolioMapper.java
+++ demo/src/main/java/com/example/demo/portfolio/mapper/PortfolioMapper.java
@@ -3,6 +3,7 @@ package com.example.demo.portfolio.mapper;
 import com.example.demo.portfolio.domain.Portfolio;
 import com.example.demo.portfolio.dto.PortfolioDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -10,5 +11,6 @@ public interface PortfolioMapper {
     PortfolioMapper INSTANCE = Mappers.getMapper(PortfolioMapper.class);
 
     PortfolioDTO.Response entityToResponse(Portfolio portfolio);
+    @Mapping(target = "id", ignore = true)
     Portfolio requestToEntity(PortfolioDTO.Request portfolioRequest);
 }
