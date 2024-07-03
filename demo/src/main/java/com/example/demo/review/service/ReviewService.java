9e56f20b17c93e9b1cdf0451a8ac4cbb3513f27d
yeong-hwan <jghff700@naver.com>
2024-07-03 11:21:26 +0900

    feat: implement simple crud for review

--- demo/src/main/java/com/example/demo/review/service/ReviewService.java
+++ demo/src/main/java/com/example/demo/review/service/ReviewService.java
@@ -1,15 +1,104 @@
 package com.example.demo.review.service;
 
-import com.example.demo.portfolio.mapper.PortfolioMapper;
+import com.example.demo.config.S3Uploader;
+import com.example.demo.portfolio.domain.Portfolio;
+import com.example.demo.portfolio.dto.PortfolioDTO;
+import com.example.demo.review.domain.Review;
+import com.example.demo.review.dto.ReviewDTO;
 import com.example.demo.review.mapper.ReviewMapper;
 import com.example.demo.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final ReviewMapper reviewMapper = ReviewMapper.INSTANCE;
+
+    private final S3Uploader s3Uploader;
+
+    public ReviewDTO.Response getReviewById(Long id) {
+        Review review = reviewRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Review not found"));
+
+        List<String> CloudFrontImageUrl = s3Uploader.getImageUrls(review.getWeddingPhotoUrls());
+        review.setWeddingPhotoUrls(CloudFrontImageUrl);
+
+        return reviewMapper.entityToResponse(review);
+    }
+
+    @Transactional
+    public ReviewDTO.Response createReview(ReviewDTO.Request reviewRequest) {
+        //Change image name to be unique
+        reviewRequest.setWeddingPhotoUrls(reviewRequest.getWeddingPhotoUrls().stream()
+                .map(s3Uploader::getUniqueFilename)
+                .collect(Collectors.toList()));
+
+        //Upload image to s3
+        List<String> presignedUrlList = s3Uploader.uploadFileList(reviewRequest.getWeddingPhotoUrls());
+
+        //save preview, set presigned url and cloudfront url to response
+        Review review = reviewMapper.requestToEntity(reviewRequest);
+        review = reviewRepository.save(review);
+        ReviewDTO.Response response = reviewMapper.entityToResponse(review);
+
+        response.setPresignedWeddingPhotoUrls(presignedUrlList);
+
+        response.setWeddingPhotoUrls(review.getWeddingPhotoUrls().stream()
+                .map(s3Uploader::getImageUrl)
+                .collect(Collectors.toList()));
+
+        return response;
+    }
+
+    @Transactional
+    public ReviewDTO.Response updatePortfolio(Long id, ReviewDTO.Request reviewRequest) {
+        Review existingReview = reviewRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Review not found"));
+
+        List<String> weddingPhotoUrls = existingReview.getWeddingPhotoUrls();
+        if (weddingPhotoUrls != null) {
+            //Delete existing images from s3 and upload new images
+            weddingPhotoUrls.forEach(s3Uploader::deleteFile);
+            existingReview.setWeddingPhotoUrls(reviewRequest.getWeddingPhotoUrls().stream()
+                    .map(s3Uploader::getUniqueFilename)
+                    .collect(Collectors.toList()));
+            s3Uploader.uploadFileList(reviewRequest.getWeddingPhotoUrls());
+        }
+
+        //save review, set presigned url and cloudfront url to response
+        Review updatedReview = reviewMapper.updateFromRequest(reviewRequest, existingReview);
+
+        Review savedReview= reviewRepository.save(updatedReview);
+        ReviewDTO.Response response = reviewMapper.entityToResponse(savedReview);
+        response.setPresignedWeddingPhotoUrls(updatedReview.getWeddingPhotoUrls());
+
+        response.setWeddingPhotoUrls(updatedReview.getWeddingPhotoUrls().stream()
+                .map(s3Uploader::getImageUrl)
+                .collect(Collectors.toList()));
+
+        return reviewMapper.entityToResponse(savedReview);
+    }
+
+
+    @Transactional
+    public void deleteReview(Long id) {
+        Review review = reviewRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Review not found"));
+
+        List<String> weddingPhotoUrls = review.getWeddingPhotoUrls();
+
+        if (weddingPhotoUrls != null) {
+            s3Uploader.deleteFiles(weddingPhotoUrls);
+        }
+
+        reviewRepository.delete(review);
+    }
+
 
 }
