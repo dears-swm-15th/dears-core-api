2f24548b7e61ee3270cb52b8043c3257fd88eb25
yeong-hwan <jghff700@naver.com>
2024-07-23 19:01:19 +0900

    fix: resolve exist branch condition

--- demo/src/main/java/com/example/demo/chat/controller/ChatRoomController.java
+++ demo/src/main/java/com/example/demo/chat/controller/ChatRoomController.java
@@ -7,10 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,8 +19,8 @@ public class ChatRoomController {
     private final ChatRoomService chatRoomService;
 
     @PostMapping("/{portfolioId}")
-    @Operation(summary = "포트폴리오 아이디로 채팅방 입장(생성 및 입장)")
-    public ResponseEntity<ChatRoomDTO.Response> enterChatRoomByPortfolioId(Long portfolioId) {
+    @Operation(summary = "[신랑신부] 포트폴리오 아이디로 채팅방 입장(생성 및 입장)")
+    public ResponseEntity<ChatRoomDTO.Response> enterChatRoomByPortfolioId(@PathVariable Long portfolioId) {
         ChatRoomDTO.Response createdChatRoom = chatRoomService.enterChatRoomByPortfolioId(portfolioId);
         return ResponseEntity.status(201).body(createdChatRoom);
     }
