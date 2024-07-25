a1ac91e7bca6f339e5c6277d81d88737ce65f815
전영서 <20wjsdudtj@gmail.com>
2024-07-25 11:20:31 +0900

    fix/standardize controller urls in swagger

--- demo/src/main/java/com/example/demo/chat/controller/ChatRoomController.java
+++ demo/src/main/java/com/example/demo/chat/controller/ChatRoomController.java
@@ -13,32 +13,32 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/chatroom")
 @Tag(name = "chatroom", description = "채팅방 API")
 public class ChatRoomController {
     private final ChatRoomService chatRoomService;
 
-    @PostMapping("/customer/chatroom/{portfolioId}")
+    @PostMapping("/customer/{portfolioId}")
     @Operation(summary = "[신랑신부] 포트폴리오 아이디로 채팅방 입장(생성 및 입장)")
     public ResponseEntity<ChatRoomDTO.Response> enterChatRoomByPortfolioIdForCustomer(@PathVariable Long portfolioId) {
         ChatRoomDTO.Response createdChatRoom = chatRoomService.enterChatRoomByPortfolioId(portfolioId);
         return ResponseEntity.status(201).body(createdChatRoom);
     }
-    @PostMapping("/weddingplanner/chatroom/{chatRoomId}")
+    @PostMapping("/weddingplanner/{chatRoomId}")
     @Operation(summary = "[웨딩플래너] 채팅방 아이디로 채팅방 입장")
     public ResponseEntity<ChatRoomDTO.Response> getChatRoomByIdForWeddingPlanner(@PathVariable Long chatRoomId) {
         ChatRoomDTO.Response chatRoomByChatRoomId = chatRoomService.getChatRoomByChatRoomId(chatRoomId);
         return ResponseEntity.status(200).body(chatRoomByChatRoomId);
     }
 
-    @GetMapping("/customer/chatroom/get")
+    @GetMapping("/customer/all")
     @Operation(summary = "[신랑신부] 현재 모든 채팅방 조회")
     public ResponseEntity<List<ChatRoomOverviewDTO.Response>> getAllChatRoomForCustomer() {
         List<ChatRoomOverviewDTO.Response> currentUsersAllChatRoom = chatRoomService.getCustomersAllChatRoom();
         return ResponseEntity.status(200).body(currentUsersAllChatRoom);
     }
 
-    @GetMapping("/weddingplanner/chatroom/get")
+    @GetMapping("/weddingplanner/all")
     @Operation(summary = "[웨딩플래너] 현재 모든 채팅방 조회")
     public ResponseEntity<List<ChatRoomOverviewDTO.Response>> getAllChatRoomForWeddingPlanner() {
         List<ChatRoomOverviewDTO.Response> currentUsersAllChatRoom = chatRoomService.getWeddingPlannersAllChatRoom();
@@ -47,7 +47,7 @@ public class ChatRoomController {
 
     // TODO : 현재 유저만 나가야 함.
     // TODO : 메소드 명, API route 변경
-    @PostMapping("/delete/{chatRoomId}")
+    @PostMapping("/shared/delete/{chatRoomId}")
     @Operation(summary = "특정 채팅방 나가기")
     public ResponseEntity<Void> deleteChatRoom(Long chatRoomId) {
         chatRoomService.deleteChatRoom(chatRoomId);
--- demo/src/main/java/com/example/demo/portfolio/controller/PortfolioController.java
+++ demo/src/main/java/com/example/demo/portfolio/controller/PortfolioController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/portfolio")
 @Tag(name = "portfolio", description = "포트폴리오 API")
 public class PortfolioController {
 
@@ -23,7 +23,7 @@ public class PortfolioController {
 
     private final PortfolioSearchService portfolioSearchService;
 
-    @GetMapping("/portfolio/get/{id}")
+    @GetMapping("/shared/{id}")
     @Operation(summary = "[공통] 특정 포트폴리오 조회")
     public ResponseEntity<PortfolioDTO.Response> getPortfolioById(
             @Parameter(description = "portfolioId")
@@ -32,14 +32,14 @@ public class PortfolioController {
         return ResponseEntity.ok(portfolioResponse);
     }
 
-    @PostMapping("/weddingplanner/portfolio/create")
+    @PostMapping("/weddingplanner/create")
     @Operation(summary = "[웨딩플래너] 포트폴리오 작성")
     public ResponseEntity<PortfolioDTO.Response> createPortfolio(@RequestBody PortfolioDTO.Request portfolioRequest) {
         PortfolioDTO.Response createdPortfolio = portfolioService.createPortfolio(portfolioRequest);
         return ResponseEntity.status(201).body(createdPortfolio);
     }
 
-    @PostMapping("/weddingplanner/portfolio/update/{id}")
+    @PostMapping("/weddingplanner/update/{id}")
     @Operation(summary = "[웨딩플래너] 특정 포트폴리오 업데이트")
     public ResponseEntity<PortfolioDTO.Response> updatePortfolio(
             @Parameter(description = "portfolioId")
@@ -48,7 +48,7 @@ public class PortfolioController {
         return ResponseEntity.ok(updatedPortfolio);
     }
 
-    @PostMapping("/weddingplanner/portfolio/delete/{id}")
+    @PostMapping("/weddingplanner/delete/{id}")
     @Operation(summary = "[웨딩플래너] 특정 포트폴리오 삭제")
     public ResponseEntity<Void> deletePortfolio(
             @Parameter(description = "portfolioId")
@@ -57,21 +57,21 @@ public class PortfolioController {
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/portfolio/get/soft-deleted")
+    @GetMapping("/shared/soft-deleted")
     @Operation(summary = "[공통] soft-deleted가 된 포트폴리오 조회")
     public ResponseEntity<List<PortfolioDTO.Response>> getAllSoftDeleted() {
         List<PortfolioDTO.Response> softDeletedPortfolios = portfolioService.getAllSoftDeletedPortfolios();
         return ResponseEntity.ok(softDeletedPortfolios);
     }
 
-    @GetMapping("/portfolio/get")
+    @GetMapping("/shared/all")
     @Operation(summary = "[공통] soft-deleted 를 포함한 전체 포트폴리오 조회")
     public ResponseEntity<List<PortfolioDTO.Response>> getAllPortfolios() {
         List<PortfolioDTO.Response> portfolioResponses = portfolioService.getAllPortfolios();
         return ResponseEntity.ok(portfolioResponses);
     }
 
-    @GetMapping("/portfolio/search")
+    @GetMapping("/shared/search")
     @Operation(summary = "[공통] 포트폴리오 검색하여 조회")
     public ResponseEntity<List<PortfolioSearchDTO.Response>> getSearchPortfolio(
             @Parameter(description = "검색 키워드")
