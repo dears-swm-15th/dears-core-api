fd7ae5078ca0383b8a0fe065264a56d317647d52
yeong-hwan <jghff700@naver.com>
2024-07-29 16:32:33 +0900

    feat(member): append sl4fj logger at api logic

--- demo/src/main/java/com/example/demo/member/controller/MemberController.java
+++ demo/src/main/java/com/example/demo/member/controller/MemberController.java
@@ -1,11 +1,12 @@
 package com.example.demo.member.controller;
 
-import  com.example.demo.member.dto.AuthDTO;
+import com.example.demo.member.dto.AuthDTO;
 import com.example.demo.member.dto.MypageDTO;
 import com.example.demo.member.service.CustomUserDetailsService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.*;
@@ -14,30 +15,33 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
 @Tag(name = "member", description = "멤버 API")
+@Slf4j
 public class MemberController {
 
     private final CustomUserDetailsService customUserDetailsService;
 
     @PostMapping("/auth/shared/create")
     @Operation(summary = "[공통] 토큰이 없을 때 유저 생성", description = "CUSTOMER 또는 WEDDINGPLANNER로 권한을 요청합니다.")
-    public ResponseEntity<AuthDTO.Response> createMember(@RequestBody AuthDTO.Request customerAuthRequest){
+    public ResponseEntity<AuthDTO.Response> createMember(@RequestBody AuthDTO.Request customerAuthRequest) {
         AuthDTO.Response createdMember = customUserDetailsService.join(customerAuthRequest.getRole());
+        log.info("Created new member with role: {}", customerAuthRequest.getRole());
         return ResponseEntity.status(201).body(createdMember);
     }
 
     @ExceptionHandler(MissingRequestHeaderException.class)
     @GetMapping("/mypage/customer/me")
     @Operation(summary = "[신랑신부] 마이페이지 조회", description = "토큰을 통해 마이페이지 정보를 조회합니다.")
-    public ResponseEntity<MypageDTO.CustomerResponse> getCustomerMyPage(){
+    public ResponseEntity<MypageDTO.CustomerResponse> getCustomerMyPage() {
         MypageDTO.CustomerResponse myPage = customUserDetailsService.getCustomerMyPage();
+        log.info("Fetched customer my page");
         return ResponseEntity.status(200).body(myPage);
     }
 
     @GetMapping("/mypage/weddingplanner/me")
     @Operation(summary = "[웨딩플래너] 마이페이지 조회", description = "토큰을 통해 마이페이지 정보를 조회합니다.")
-    public ResponseEntity<MypageDTO.WeddingPlannerResponse> getWeddingPlannerMyPage(){
+    public ResponseEntity<MypageDTO.WeddingPlannerResponse> getWeddingPlannerMyPage() {
         MypageDTO.WeddingPlannerResponse myPage = customUserDetailsService.getWeddingPlannerMyPage();
+        log.info("Fetched wedding planner my page");
         return ResponseEntity.status(200).body(myPage);
     }
-
 }
