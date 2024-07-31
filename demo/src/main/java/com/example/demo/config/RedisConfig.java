202decbaf392164dd1dfb86bc75c4615bc6dd402
yeong-hwan <jghff700@naver.com>
2024-07-31 11:35:30 +0900

    feat: implement redis usage methods

--- demo/src/main/java/com/example/demo/config/RedisConfig.java
+++ demo/src/main/java/com/example/demo/config/RedisConfig.java
@@ -21,15 +21,11 @@ public class RedisConfig {
     @Value("${spring.data.redis.host}")
     private String host;
 
-//    @Value("${spring.data.redis.password}")
-//    private String password;
-
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
         redisConfiguration.setHostName(host);
         redisConfiguration.setPort(port);
-//        redisConfiguration.setPassword(password);
         return new LettuceConnectionFactory(redisConfiguration);
     }
 
