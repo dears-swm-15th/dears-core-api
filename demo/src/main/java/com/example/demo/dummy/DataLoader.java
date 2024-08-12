d3839d11a8b0b6a0eeadf1fae2367a7c9d7ba0da
yeong-hwan <jghff700@naver.com>
2024-08-12 20:10:36 +0900

    refactor: delete useless variable dependency

--- demo/src/main/java/com/example/demo/dummy/DataLoader.java
+++ demo/src/main/java/com/example/demo/dummy/DataLoader.java
@@ -50,9 +50,6 @@ public class DataLoader implements CommandLineRunner {
 
     @Autowired
     private ChatRoomRepository chatRoomRepository;
-  
-    @Autowired
-    private PortfolioSearchService portfolioSearchService;
 
     @Override
     public void run(String... args) throws Exception {
