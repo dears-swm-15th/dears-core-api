42e12b087c698e31e2ff821d56ec834d25fb5d86
yeong-hwan <jghff700@naver.com>
2024-08-26 15:38:38 +0900

    refactor: enhance customer service message with additional metadata
    - Updated `DiscordMessageProvider` to include customer information and a timestamp when sending customer service messages to Discord.
    - Added new records (`Author`, `Field`, `Footer`, `Image`, `Thumbnail`) to structure the Discord message with enhanced metadata.

--- demo/src/main/java/com/example/demo/discord/DiscordMessageProvider.java
+++ demo/src/main/java/com/example/demo/discord/DiscordMessageProvider.java
@@ -1,11 +1,15 @@
 package com.example.demo.discord;
 
 import com.example.demo.discord.event.DiscordFeignCustomerService;
+import com.example.demo.member.domain.Customer;
+import com.example.demo.member.dto.MypageDTO;
 import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import static com.example.demo.discord.DiscordMessage.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -13,8 +17,32 @@ public class DiscordMessageProvider {
 
     private final DiscordFeignCustomerService discordFeignCustomerService;
 
-    public void sendCustomerServiceMessage(String message) {
-        DiscordMessage discordMessage = createCustomerServiceMessage(message);
+    public void sendCustomerServiceMessage(Customer customer, MypageDTO.CustomerServiceRequest request) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedTimestamp = LocalDateTime.now().format(formatter);
+
+        // Example embed creation
+        DiscordMessage.Embed embed = new DiscordMessage.Embed(
+                request.getTitle(),          // title
+                request.getContent(),        // description
+                4886754,                     // color (Cyan)
+                new DiscordMessage.Footer(formattedTimestamp),
+//                new DiscordMessage.Image("https://example.com/image.png"),
+//                new DiscordMessage.Thumbnail("https://example.com/thumbnail.png"),
+                new DiscordMessage.Author(customer.getName() + "(" +  customer.getUUID() + ")")
+//                List.of(
+//                        new DiscordMessage.Field("Date", "date", true),
+//                        new DiscordMessage.Field("Time", "time", true)
+//                )
+        );
+
+        // Creating a Discord message with the embed
+        DiscordMessage discordMessage = DiscordMessage.createCustomerServiceMessage(
+                EventMessage.CUSTOMER_SERVICE.getMessage(),
+                List.of(embed)
+        );
+
         sendCustomerMessageToDiscord(discordMessage);
     }
 
