f2cc92160f168e3557af0192f383dbbb4a0df25e
yeong-hwan <jghff700@naver.com>
2024-09-11 15:15:50 +0900

    test: ChatRoomRepository - 웨딩플래너 ID로 마지막 메세지가 생성된 순서로 채팅방을 조회한다

--- demo/src/test/java/com/example/demo/chat/repository/ChatRoomRepositoryTest.java
+++ demo/src/test/java/com/example/demo/chat/repository/ChatRoomRepositoryTest.java
@@ -5,6 +5,8 @@ import com.example.demo.config.S3Uploader;
 import com.example.demo.member.domain.Customer;
 import com.example.demo.member.domain.WeddingPlanner;
 import com.example.demo.member.repository.CustomerRepository;
+import com.example.demo.member.repository.WeddingPlannerRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -32,6 +35,8 @@ class ChatRoomRepositoryTest {
 
     @Autowired
     private CustomerRepository customerRepository;
+    @Autowired
+    private WeddingPlannerRepository weddingPlannerRepository;
 
     @Test
     @DisplayName("고객 ID로 채팅방을 조회한다.")
@@ -126,11 +131,15 @@ class ChatRoomRepositoryTest {
 
         ChatRoom chatRoom1 = ChatRoom.builder()
                 .customer(customer)
+                .lastMessageCreatedAt(LocalDateTime.parse("2000-01-01T00:00:00"))
                 .build();
 
         ChatRoom chatRoom2 = ChatRoom.builder()
                 .customer(customer)
+                .lastMessageCreatedAt(LocalDateTime.parse("2000-01-02T00:00:00"))
                 .build();
+
+        customer.setChatRooms(List.of(chatRoom1, chatRoom2));
 
         customerRepository.save(customer);
         chatRoomRepository.saveAll(List.of(chatRoom1, chatRoom2));
@@ -140,9 +149,46 @@ class ChatRoomRepositoryTest {
 
         // then
         assertThat(chatRooms.size()).isEqualTo(2);
-        assertThat(chatRooms.get(0).getCustomer().getId()).isEqualTo(customer.getId());
-        assertThat(chatRooms.get(1).getCustomer().getId()).isEqualTo(customer.getId());
+
+        // compare chatRoom1 and chatRoom2
+        assertThat(chatRooms.get(0).getLastMessageCreatedAt())
+                .isAfter(chatRooms.get(1).getLastMessageCreatedAt()); // chatRoom2 should be first because it's more recent
+
     }
 
-    
+    @Test
+    @Transactional
+    @DisplayName("웨딩플래너 ID로 마지막 메세지가 생성된 순서로 채팅방을 조회한다.")
+    void findByWeddingPlannerIdOrderByLastMessageCreatedAtDesc() {
+        // given
+        WeddingPlanner weddingPlanner = WeddingPlanner.builder()
+                .id(1L)
+                .build();
+
+        ChatRoom chatRoom1 = ChatRoom.builder()
+                .weddingPlanner(weddingPlanner)
+                .lastMessageCreatedAt(LocalDateTime.parse("2000-01-01T00:00:00"))
+                .build();
+
+        ChatRoom chatRoom2 = ChatRoom.builder()
+                .weddingPlanner(weddingPlanner)
+                .lastMessageCreatedAt(LocalDateTime.parse("2000-01-02T00:00:00"))
+                .build();
+
+        weddingPlanner.setChatRooms(List.of(chatRoom1, chatRoom2));
+
+        weddingPlannerRepository.save(weddingPlanner);
+        chatRoomRepository.saveAll(List.of(chatRoom1, chatRoom2));
+
+        // when
+        List<ChatRoom> chatRooms = chatRoomRepository.findByWeddingPlannerIdOrderByLastMessageCreatedAtDesc(weddingPlanner.getId());
+
+        // then
+        assertThat(chatRooms.size()).isEqualTo(2);
+
+        // compare chatRoom1 and chatRoom2
+        assertThat(chatRooms.get(0).getLastMessageCreatedAt())
+                .isAfter(chatRooms.get(1).getLastMessageCreatedAt()); // chatRoom2 should be first because it's more recent
+    }
+
 }
