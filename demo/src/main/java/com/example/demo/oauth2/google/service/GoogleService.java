00eac8d7ce9e777a7ce31cf05b0fb6012153edee
yeong-hwan <jghff700@naver.com>
2024-09-30 12:15:02 +0900

    fix: resolve google oauth user info retrieval
    - Changed the Google user info request method from POST to GET
    - Added 'Authorization' header with bearer token for secure API access
    - Ensured necessary scopes (e.g., 'profile') are included for retrieving user's name

--- demo/src/main/java/com/example/demo/oauth2/google/service/GoogleService.java
+++ demo/src/main/java/com/example/demo/oauth2/google/service/GoogleService.java
@@ -3,29 +3,33 @@ package com.example.demo.oauth2.google.service;
 import com.example.demo.oauth2.google.dto.GoogleUserInfoResponseDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class GoogleService {
 
-    private final String GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token";
-    private final String GOOGLE_MEMBER_INFO_URL = "https://oauth2.googleapis.com/tokeninfo";
-
     public GoogleUserInfoResponseDTO getGoogleMemberInfo(String accessToken) {
         RestTemplate restTemplate = new RestTemplate();
 
-        log.info("accessToken : {}", accessToken);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
 
-        HashMap<String, String> accessTokenMap = new HashMap<>();
-        accessTokenMap.put("access_token", accessToken);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
 
-        GoogleUserInfoResponseDTO memberInfo = restTemplate.postForObject(GOOGLE_MEMBER_INFO_URL, accessTokenMap, GoogleUserInfoResponseDTO.class);
-
-        return memberInfo;
+        ResponseEntity<GoogleUserInfoResponseDTO> response = restTemplate.exchange(
+                "https://www.googleapis.com/oauth2/v3/userinfo",
+                HttpMethod.GET,
+                entity,
+                GoogleUserInfoResponseDTO.class
+        );
+        
+        return response.getBody();
     }
 }
