cb5cdeea0c98db6d5d152abd7d473967960f1754
yeong-hwan <jghff700@naver.com>
2024-10-02 17:34:04 +0900

    refactor: append various prompting engineering techniques

--- demo/src/main/java/com/example/demo/chat/controller/ChatBotController.java
+++ demo/src/main/java/com/example/demo/chat/controller/ChatBotController.java
@@ -1,10 +1,10 @@
 package com.example.demo.chat.controller;
 
+import com.example.demo.chat.service.ChatBotService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,17 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "chatbot", description = "챗봇 API")
 public class ChatBotController {
 
-    private final ChatClient chatClient;
+    private final ChatBotService chatBotService;
 
     @PostMapping("/test")
     @Operation(summary = "테스트")
     public ResponseEntity<String> chatWithChatBot(@RequestBody String message) {
 
-        String answer = chatClient.prompt()
-                .system("넌 이제부터 한국의 웨딩플래너 전문가야. 추정치를 바탕으로 확실한 답변을 해줘.")
-                .user(message)
-                .call()
-                .content();
+        String answer = chatBotService.getAnswer(message);
 
         return ResponseEntity.status(200).body(answer);
     }
